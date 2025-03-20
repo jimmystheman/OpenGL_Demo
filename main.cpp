c87a2b872091435a52ea99a7240775101ed6494b
Jimmy Haviland <jhaviland09@gmail.com>
2025-03-20 13:56:51 -0500

    some user input to modify color

--- main.cpp
+++ main.cpp
@@ -3,6 +3,32 @@
 #include <iostream>
 using namespace std;
 
+struct ColorManager {
+    // vals need to be within 0 - 1.0
+    float r = 0.f;
+    float g = 0.f;
+    float b = 0.f;
+    float a = 0.f;
+
+    void Normalize()
+    {
+        if (r > 1.0)
+            r = 1.0;
+        if (r < 0.0)
+            r = 0.0;
+
+        if (g > 1.0)
+            g = 1.0;
+        if (g < 0.0)
+            g = 0.0;
+
+        if (b > 1.0)
+            b = 1.0;
+        if (b < 0.0)
+            b = 0.0;
+    }
+};
+
 // resize callback
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
@@ -10,14 +36,35 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 }
 
 // process input, escape = indicate window should close
-void processInput(GLFWwindow* window)
+void processInput(GLFWwindow* window, ColorManager& color)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
+
+    if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
+        color.r += 0.01;
+    else if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
+        color.r -= 0.01;
+
+    if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
+        color.g += 0.01;
+    else if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
+        color.g -= 0.01;
+
+    if (glfwGetKey(window, GLFW_KEY_RIGHT_SHIFT) == GLFW_PRESS)
+        color.b += 0.01;
+    else if (glfwGetKey(window, GLFW_KEY_RIGHT_CONTROL) == GLFW_PRESS)
+        color.b -= 0.01;
+
+    // keep colors in 0.0 - 1.0 range
+    color.Normalize();
 }
 
 int main()
 {
+    // color struct to update color w/ user input and save state
+    ColorManager clear_color;
+
     // proj set up
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -52,10 +99,11 @@ int main()
     while (!glfwWindowShouldClose(window))
     {
         // user input
-        processInput(window);
-
+        processInput(window, clear_color);
+        system("cls");
+        cout << "color [rgba] = [" << clear_color.r << ", " << clear_color.g << ", " << clear_color.b << ", " << clear_color.a << "]";
         // render
-        glClearColor(1.0f, 0.3f, 0.3f, 1.0f); // define color to set when clear called
+        glClearColor(clear_color.r, clear_color.g, clear_color.b, clear_color.a); // define color to set when clear called
         glClear(GL_COLOR_BUFFER_BIT); // clear screen, set to solid color
 
         // check events/call callbacks
